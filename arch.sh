702a9d1f7d83e812a38ced3351aefc1d30bb35ee
MR.PATRIX <yehezkielmartis1@gmail.com>
2024-06-28 09:17:43 +0700

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -60,7 +60,7 @@ CYAN='\033[1;36m'
     echo -e "${CYAN}1.${GREEN} dbus-broker-units (Khusus Android Tinggi)${NC}"
     echo -e "${CYAN}2.${GREEN} dbus-daemon-units (Khusus Android Rendah)${YELLOW}"
 echo ""
-
+wget https://raw.githubusercontent.com/PATRIX-CYBERGHOST01/Arch-xfce/main/user.sh
 		downloader "$CURR_DIR/user.sh" "https://raw.githubusercontent.com/PATRIX-CYBERGHOST01/Arch-xfce/blob/main/user.sh"
 		mv -f "$CURR_DIR/user.sh" "$UBUNTU_DIR/root/user.sh"
 		
