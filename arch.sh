c4eb6f0ba6a53fd1bbd1597e2490a115fc5e6c23
MR.PATRIX <yehezkielmartis1@gmail.com>
2024-08-24 21:11:31 +0700

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -49,8 +49,8 @@ proot-distro install archlinux
 echo 'pulseaudio --start \
     --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" \
     --exit-idle-time=-1
-proot-distro login archlinux ' > /data/data/com.termux/files/usr/bin/arch.sh
-    chmod +x /data/data/com.termux/files/usr/bin/arch.sh
+proot-distro login archlinux ' > /data/data/com.termux/files/usr/bin/arch
+    chmod +x /data/data/com.termux/files/usr/bin/arch
     
 	
 sleep 2
